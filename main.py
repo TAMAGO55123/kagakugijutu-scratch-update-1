e1a4a8b9a5ce203c6ccdc83c6ef014fa750755f1
TAMAGO551234 <tamago55.1234@gmail.com>
2024-07-27 18:53:46 +0900

    色々

---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+import requests
+import os
+
+webhook_url1 = 'https://script.google.com/macros/s/AKfycbzZ18jdzpAoMQw7q8Ie9z5ren9Y5QUdv9jAq27gvTTijmc8jLBfg0eomYZJF8JwEpof/exec?grade=1'
+webhook_url2 = 'https://script.google.com/macros/s/AKfycbzZ18jdzpAoMQw7q8Ie9z5ren9Y5QUdv9jAq27gvTTijmc8jLBfg0eomYZJF8JwEpof/exec?grade=2'
+
+r1=requests.get(url=webhook_url1)
+r2=requests.get(url=webhook_url2)
+
+print('1')
+data1=json.dumps(json.loads(r1.text), ensure_ascii=False, indent=4)
+print(data1)
+
+print('\n2')
+data2=json.dumps(json.loads(r2.text), ensure_ascii=False, indent=4)
+print(data2)
+
+data1s=data1.replace('\n', '')
+data1s=data1s.replace(' ', '')
+data2s=data2.replace('\n', '')
+data2s=data2s.replace(' ', '')
+print('var gradedata=['+data1s+','+data2s+'];')
+
+os.system("git pull")
+os.system("git config user.name TAMAGO551234")
+os.system("git config user.email tamago55.1234@gmail.com")
