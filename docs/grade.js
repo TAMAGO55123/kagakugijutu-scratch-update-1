1612867d59547ec5b0e14645e926f22bef84a824
TAMAGO551234 <tamago55.1234@gmail.com>
2024-07-28 14:09:12 +0900

    update grade files

--- docs/grade.js
+++ docs/grade.js
@@ -1,1 +1,1 @@
-var gradedata=[{"grade":"1","ID":[[552154309]],"Name":[["てsと"]],"length":1},{"grade":"2","ID":[[366353052]],"Name":[["スイカゲームチート版"]],"length":1}];
+var gradedata=[{"grade":"1","ID":[[886142060]],"Name":[["おにぎりガチャ!!"]],"length":1},{"grade":"2","ID":[[366353052]],"Name":[["スイカゲームチート版"]],"length":1}];
