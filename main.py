df222055d8d6a0bf5e135270deb58750d3bde99b
TAMAGO55 <147392915+TAMAGO55123@users.noreply.github.com>
2025-03-12 02:20:57 +0900

    Add comment

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+# 拡張機能を読み込む
 import json
 import requests
 import os
 
+# データベースの取得URLを指定
 webhook_url1 = 'https://script.google.com/macros/s/AKfycbzZ18jdzpAoMQw7q8Ie9z5ren9Y5QUdv9jAq27gvTTijmc8jLBfg0eomYZJF8JwEpof/exec?grade=1'
 #webhook_url1=os.getenv('webhook1')
 webhook_url2 = 'https://script.google.com/macros/s/AKfycbzZ18jdzpAoMQw7q8Ie9z5ren9Y5QUdv9jAq27gvTTijmc8jLBfg0eomYZJF8JwEpof/exec?grade=2'
 #webhook_url2=os.getenv('webhook2')
 
+# データベースからデータを取得
 print('r1')
 r1 = requests.get(url=webhook_url1)
 print('r2')
 r2 = requests.get(url=webhook_url2)
 
+# データを変換する
 print('1')
 data1 = json.dumps(json.loads(r1.text), ensure_ascii=False, indent=4)
 print(data1)
@@ -27,15 +31,19 @@ data2s = data2s.replace(' ', '')
 datag = 'var gradedata=[' + data1s + ',' + data2s + '];'
 print(datag)
 
+# Webサーバーのデータの最新版をゲットする
 print("git pull")
 os.system("git pull")
+# コマンドの設定
 os.system("git config user.name TAMAGO55123")
 os.system("git config user.email tamago.55123@gmail.com")
 
+# 書き込み
 print("write grade.js")
 with open('docs/grade.js', mode='w') as f:
     f.write(datag)
 
+# アップロード
 #os.system("cd Kagakugijutu-Scratch")
 print("git add .")
 os.system("git add .")
